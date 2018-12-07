28fc56b5cc36a7babea52b2eb4b69a93e4efa559
Ely Alamillo <ely0013@gmail.com>
2018-12-06 23:11:21 -0600

    fixed typo in Auth import

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ from flask_jwt_extended import (
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from resource.auth import Auth
+from resources.auth import Auth
 
 
 app = Flask(__name__)
